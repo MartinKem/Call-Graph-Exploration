a88ac3e43e8b59586976b97d4053020883ca9d99
R98 <robert.sauerborn@web.de>
2018-11-20 12:41:20 +0100

    Add files via upload
    Funktion die jeden knoten mit eigenen Eigenschaften erstellt

--- Beispielgraph/script.js
+++ Beispielgraph/script.js
@@ -1,1 +1,27 @@
-.
+
+function createNodes(nodes,foreign) {
+    nodes.forEach(foo);
+    function foo(n) {
+        console.log(n.name + "-" + n.inhalt);
+        var x=foreign
+            .append("xhtml:div")
+            .attr("class","div_node")
+            .style("left", n.x + "px")
+            .style("top", n.y + "px")
+            .style("width", n.width + "px")
+            .style("height", n.height + "px")
+            .append("xhtml:h3")
+            .text(n.name)
+            .append("xhtml:div")
+            .attr("class","node_inhalt")
+            .append("xhtml:ul")
+
+            for(var i=0;i!=n.inhalt.length;i++){
+            x
+                .append("xhtml:li")
+                .text(i+ ": " + n.inhalt[i])
+            }
+
+    }
+}
+
