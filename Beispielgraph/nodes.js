50116c99040b3765c1d2060fe2ce73b783d076f5
MartinKem <martin.kempken@online.de>
2018-11-27 23:41:06 +0100

    Umbenennung von script.js

---
+++ Beispielgraph/nodes.js
@@ -0,0 +1,40 @@
+//creates a div with header and functioncalls for each nodeelement
+//nodes = node objects
+//foreign = foreignObject (needed to create html objects inside)
+function createNodes(nodes,foreign) {
+    var j= 0;
+	nodes.forEach(foo);
+    function foo(n) {
+        var node = foreign.append("xhtml:div")
+                .attr("class","div_node")
+                .attr("id", j)
+                .style("left", n.x + "px")
+                .style("top", n.y + "px")
+                .style("min-width", "300px")
+                .style("padding", "20px");
+							
+        node.append("xhtml:h3")
+            .text(n.name)
+			.style("text-align", "center");
+			
+        node = node.append("xhtml:div")
+					.attr("class","node_inhalt")
+            for(var i=0; i!=n.inhalt.length; i++){
+            node.append("xhtml:div")
+                .text(i + ": " + n.inhalt[i])
+				.attr("id", j + "#" + n.inhalt[i])
+				.style("width", "100%")
+				.style("border", "solid")
+				.style("box-sizing", "border-box")
+				.style("border-width", "2px")
+				.style("border-top-width", (i == 0 ? "2px" : "0px"))
+				.style("border-radius", "4px")
+				.style("padding", "5px");
+            }
+
+		j++;
+    }
+
+}
+
+
