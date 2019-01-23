dba6611689c45369a53d0fdeefa429a35f3b99a0
MartinKem <martin.kempken@online.de>
2019-01-23 13:48:39 +0100

    Möglichkeit vollständigen Forcegraphen im Voraus zu berechnen

---
+++ vollständiger ForceGraph - Prototyp/edges.js
@@ -0,0 +1,293 @@
+/*
+insertes an arrow from (xStart, yStart) to (xDest, yDest) into an svg-container
+
+svg: svg-container to insert the arrow in
+xStart: x-value of the start point
+yStart: y-value of the start point
+xDest: x-value of the destination point
+yDest: y-value of the destination point
+edgeID: id of the constructed edge
+label: declared target as arrow index
+
+returns: void
+*/
+function createEdge(svg, xStart, yStart, xDest, yDest, edgeID, label, curved){
+	 var marker = svg;
+	 
+	 if(document.getElementById("markerArrow") == null){
+		 d3.select("#definitions").append("svg:marker")
+			 .attr("id", "markerArrow") 
+			 .attr("class", "arrowHead")
+			 .attr("markerWidth", "13") 
+			 .attr("markerHeight", "13")
+			 .attr("refX", "9") // distance to line
+			 .attr("refY", "4") 
+			 .attr("orient", "auto") 
+			 .append("svg:path") 
+				 .attr("d", "M5,4 L3,1 L10,4 L3,7 L5,4")
+				 .style("fill", "black");
+	 }
+	 
+	 if(label != null){
+		var marker = d3.select("#definitions")
+			.append("svg:marker")
+			.attr("id", label)
+			.attr("markerWidth", "200")
+			.attr("markerHeight", "100")
+			.attr("refX", "100")
+			.attr("refY", "50");
+			
+		marker.append("svg:rect")
+			.attr("id", label + "_background")
+			.attr("y", "45")
+			.attr("height", "10")
+			.style("fill", "white")
+			.style("stroke-width", "1")
+			.style("stroke", "black");
+			
+		marker.append("svg:text")
+			.attr("id", label + "_text")
+			.attr("y", "52")
+			.style("font-family", "Helvetica")
+			.style("font-size", "5")
+			.text(label);
+		
+		var textWidth = document.getElementById(label + "_text").getBBox().width;
+		
+		d3.select("#" + label + "_text").attr("x", 100-textWidth/2);
+		d3.select("#" + label + "_background").attr("x", 96-textWidth/2).attr("width", textWidth+6);
+	 }
+	 
+	 var path = "M" + xStart + "," + yStart + "L" + xDest + "," + yDest;
+	 if(curved) path = getCurvedPath(xStart, yStart, xStart-27, yStart-50);
+	 
+	 svg.append("svg:path") 
+		 .attr("d", path)
+		 .attr("id", edgeID)
+		 .attr("class", "edge")
+		 .style("stroke", "black") 
+		 .style("stroke-width", "3px") 
+		 .style("fill", "none") 
+		 .style("marker-end", "url(#markerArrow)")
+		 .style("marker-mid", "url(#" + label + ")")
+		 .style("opacity", "0.5")
+		 .style("display", "block")
+		 .style("visibility", "visible");
+ }
+
+function getCurvedPath(xStart, yStart, xDest, yDest){
+	return "M " + xStart + " " + (yStart+15).toString() + 
+		   " C " + (xStart-150).toString() + " " + (yStart+50).toString() + 
+		   ", " + (xDest-120).toString() + " " + (yDest-50).toString() + 
+		   ", " + (xDest).toString() + " " + (yDest).toString();
+}
+ 
+/*
+ returns the intersection between a center-to-center line of two rectangles
+ (given as an origin, width and height) and the border of the first rectangle
+ 
+ node1{x, y, width, height}: first rectangle with result point on its border
+ node2{x, y, width, height}: second rectangle (but only center is relevant)
+ 
+ returns: {x, y} as intersection
+*/
+function borderPoint(node1, node2){
+	var center1 = {x: node1.x + node1.width/2, y: node1.y + node1.height/2};
+	var center2 = {x: node2.x + node2.width/2, y: node2.y + node2.height/2};
+	
+	var xRes = 0, yRes = 0;
+	
+	var f1 = function(x){
+		return (node1.height/node1.width) * (x - center1.x) + center1.y;
+	}
+	
+	var f2 = function(x){
+		return -(node1.height/node1.width) * (x - center1.x) + center1.y;
+	}
+		
+	var lineFunction = function(value, ret){ // f(x) = m*x + b -> center-to-center line function
+		if(center2.x-center1.x == 0){
+		    var m = (center2.y - center1.y) / 0.0000000001
+        } else {
+            var m = (center2.y - center1.y) / (center2.x - center1.x);
+
+        }
+		var b = (center2.y) - m*(center2.x);
+		if(ret === "y") return m*value + b; // return y-value of given x-value (= m*x + b)
+		if(ret === "x") return (value - b)/m; // return x-value of given y-value (= (f(x) - b)/m)
+	}
+	
+	if(center2.y >= f1(center2.x)){
+		if(center2.y >= f2(center2.x)){
+			yRes = node1.y + node1.height;	// case bottom
+			xRes = lineFunction(yRes, "x");
+		} else {
+			xRes = node1.x;					// case left
+			yRes = lineFunction(xRes, "y");
+		}
+	} else {
+		if(center2.y >= f2(center2.x)){
+			xRes = node1.x + node1.width;	// case right
+			yRes = lineFunction(xRes, "y");
+		} else {
+			yRes = node1.y;					// case top
+			xRes = lineFunction(yRes, "x");
+		}
+	}
+	return {x: xRes, y: yRes};
+}
+
+/*
+ returns the border point on the left or the right border
+ 
+ node1{x, y, width, height}: first rectangle with result point on its border
+ node2{x, y, width, height}: second rectangle (but only center is relevant)
+ 
+ returns: {x, y} as side point
+*/
+function sidePoint(node1, node2){
+	var xRes;
+	if(node2.x + node2.width/2 > node1.x + node1.width){
+		xRes = node1.x + node1.width;
+	}
+	else xRes = node1.x;
+	// if(node2.x + node2.width/2 < node1.x){
+		// xRes = node1.x;
+	// }
+	// else{
+		// return borderPoint(node1, node2);
+	// }
+	return {x: xRes, y: node1.y + node1.height/2};
+}
+
+
+/*
+creates border-to-borer edge on an imaginary center-to-center edge between two rectangles
+(available as link) into an svg-container
+
+svg: svg-container to insert the edge in
+link{source, dest}: source node and destination node
+edgeID: id of the constructed edge
+
+returns: void
+*/
+function center2centerEdge(svg, link, edgeID){
+	var n1 = borderPoint(link.source, link.dest);
+	var n2 = borderPoint(link.dest, link.source);
+	createEdge(svg, n1.x, n1.y, n2.x, n2.y, edgeID);
+}
+
+/*
+creates borde-to-border edge from the left or the right side of the source node to the center
+of the destination node
+
+svg: svg-container to insert the edge in
+link{source, dest}: source node and destination node
+edgeID: id of the constructed edge
+
+returns: void
+*/
+function side2centerEdge(svg, link, edgeID){
+	var n1 = sidePoint(link.source, link.dest);
+	var n2 = borderPoint(link.dest, link.source);
+	createEdge(svg, n1.x, n1.y, n2.x, n2.y, edgeID, "declaredTarget");
+}
+
+/*
+returns the absolute dimensions of an element anywhere in the graph
+
+id: id of the element
+
+returns: {xPos, yPos, width, height}
+*/
+function absPosition(id){
+	var element = document.getElementById(id);
+	var widthVal = element.offsetWidth;
+	var heightVal = element.offsetHeight;
+	var xVal = 0, yVal = 0;
+    do {
+		var borderwidth = 0;
+		if(element != document.getElementById(id)) borderwidth = parseInt(element.style.borderWidth, 10) || 0;
+        yVal += element.offsetTop + borderwidth || 0;
+        xVal += element.offsetLeft + borderwidth || 0;
+        element = element.offsetParent;
+    } while(element);
+	return {x: xVal, y: yVal, width: widthVal, height: heightVal}
+}
+
+/*
+creates a center2centerEdge from one element to another
+nodes must exist in the graph before edges can be created
+
+id1: id of the source element
+id2: id of the destination element
+
+returns: void
+*/
+function node2nodeEdge(id1, id2){
+	var link = {source: absPosition(id1), dest: absPosition(id2)}
+	center2centerEdge(svgCont, link, id1 + "->" + id2);
+}
+
+/*
+creates a side2centerEdge from one element to another
+nodes must exist in the graph before edges can be created
+
+id1: id of the source element
+id2: id of the destination element
+
+returns: void
+*/
+function method2nodeEdge(id1, id2){
+	var link = {source: absPosition(id1), dest: absPosition(id2)}
+	if(id1.split("#")[0] == id2) createEdge(svgCont, link.source.x, link.source.y, -1, -1, id1 + "->" + id2, null, true);
+	else side2centerEdge(svgCont, link, id1 + "->" + id2);
+}
+
+/*
+changes the path of a given edge so that it starts from the outer border of it's source node
+
+id: id of the edge
+
+returns: void
+*/
+function toggleToAbstract(id){
+	var edge = document.getElementById(id);
+	[sourceID, destID] = id.split("->");
+	var sourceID = sourceID.split("#")[0];
+	var link = {source: absPosition(sourceID), dest: absPosition(destID)};
+	if(sourceID == destID) edge.setAttribute("d", getCurvedPath(link.source.x, link.source.y+60, link.source.x, link.source.y+30));
+	else{
+		var n1 = borderPoint(link.source, link.dest);
+		var n2 = borderPoint(link.dest, link.source);
+		if(n1.x && n2.x) edge.setAttribute("d", "M" + n1.x + "," + n1.y + "L" + n2.x + "," + n2.y);
+	}
+		
+	/* uncomment this to activate edge labeling
+	var xMid = (n2.x+n1.x)/2;
+	var yMid = (n2.y+n1.y)/2;
+	var mid = "L" + xMid + "," + yMid;
+		
+	edge.setAttribute("d", "M" + n1.x + "," + n1.y + mid + "L" + n2.x + "," + n2.y);*/
+}
+
+/*
+changes the path of a given edge so that it starts from the border of it's source nodes's inner mathod div
+
+id: id of the edge
+
+returns: void
+*/
+function toggleToDetailed(id){
+	var edge = document.getElementById(id);
+	[sourceID, destID] = id.split("->");
+	var link = {source: absPosition(sourceID), dest: absPosition(destID)};
+	var n1 = sidePoint(link.source, link.dest);
+	if(n1.x && sourceID.split('#')[0] == destID) edge.setAttribute("d", getCurvedPath(n1.x, n1.y-15, n1.x-27, n1.y-65));
+	else{
+		var n2 = borderPoint(link.dest, link.source);
+		if(n1.x && n2.x) edge.setAttribute("d", "M" + n1.x + "," + n1.y + "L" + n2.x + "," + n2.y);
+	}
+}
+
+
---
+++ vollständiger ForceGraph - Prototyp/rightclickmenu.js
@@ -0,0 +1,72 @@
+
+var clickedDiv;
+var menuIsOpen = false;
+
+//eventhandler for normal leftclick, deaktivates rightclickmenu
+$("html").on("click", function(e){
+    if(menuIsOpen){
+        $("#main-rightclick").remove();
+        menuIsOpen = false;
+    }
+});
+//eventhandler for rightclick, deactivates rightclickmenu (not in .div_node)
+$("html:not(.div_node)").on("contextmenu",function(e){
+    if(menuIsOpen){
+        $("#main-rightclick").remove();
+        menuIsOpen = false;
+    }
+});
+//on rightclick in .div_node calls rightclickmenu and deactivates normal contextmenu
+//not used anymore
+$(".div_node").contextmenu(function(e) {
+    if(menuIsOpen){
+        $("#main-rightclick").remove();
+        menuIsOpen = false;
+    }
+    clickedDiv = this;
+    rightclickmenu(e);
+    return false;
+});
+//loads rightclickmenu.html on current mouse position
+function rightclickmenu(e) {
+
+
+    let x = e.pageX + "px";     // Get the horizontal coordinate
+    let y = e.pageY + "px";     // Get the vertical coordinate
+    //let link = "https://raw.githubusercontent.com/MartinKem/Call-Graph-Exploration/developer's/eingelesener%20Graph/rightclickmenu.html?token=gAYfhzzRW1xhwgU-GLzFnB5r3gtbBHuFpks5cRbzjwA%3D%3D";
+   // let counter = 0;
+    /*try{
+        //$("body").append($("<div id='main-rightclick'></div>").load(link +" #main-rightclick>"));
+
+    }catch (e) {*/
+       // counter = 1;
+       // if(counter > 0)console.log("contextmenu nicht mehr aktuell");
+        $("body").append($("<div id='main-rightclick'>        <div class=\"menuelement\" onclick=\"deleteNodes()\">Ausblenden</div>\n" +
+            "        <div class=\"menuelement\" onclick=\"colorChosen(this)\">Red<div class=\"color\" style=\"background-color: #ffc6c6 \"></div> </div>\n" +
+            "        <div class=\"menuelement\" onclick=\"colorChosen(this)\">Green<div class=\"color\" style=\"background-color: #beffbe\"></div></div>\n" +
+            "        <div class=\"menuelement\" onclick=\"colorChosen(this)\">Blue<div class=\"color\" style=\"background-color: #abd3ff\"></div></div>\n" +
+            "        <div class=\"menuelement\" onclick=\"colorChosen(this)\">Yellow<div class=\"color\" style=\"background-color: #ffff9f\"></div></div>\n" +
+            "        <div class=\"menuelement\" onclick=\"colorChosen(this)\">White<div class=\"color\" style=\"background-color: white\"></div></div></div>"));
+
+    $("#main-rightclick").css({
+        "position":"absolute",
+        "top":y,
+        "left":x,});
+
+
+    menuIsOpen = true;
+
+}
+//changes color to the backgroundcolor of elem
+function colorChosen(elem) {
+    var color = $(elem).find(".color").css('backgroundColor');
+    $(clickedDiv).css('background-color', color);
+}
+
+
+function deleteNodes() {
+    var nodeId= $(clickedDiv).attr('id');
+    nodeId = parseInt(nodeId);
+	var nodeInstance = getNodeById(nodeId, rootNode);
+	nodeInstance.hideNode();
+}
