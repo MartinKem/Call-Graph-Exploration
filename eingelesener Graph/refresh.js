20a470b3255e3d33f11775a37485a205f6524bb8
FlorianTreede <floriantreede@yahoo.de>
2019-02-06 11:05:13 +0100

    hinzufügen von Graphenstatistikenanzeige

---
+++ eingelesener Graph/refresh.js
@@ -0,0 +1,11 @@
+function refreshGraphData{
+document.getElementById("totalNodes").innerHTML = "Total Nodes" + String(createdNodes);
+document.getElementById("totalEdges").innerHTML = "Total Edges" + "placeholder";
+document.getElementById("currentVisibleNodes").innerHTML = "Current Nodes" + String(currentNodes);
+document.getElementById("currentVisibleEdges").innerHTML = "Current Edges" + "0";
+}
+
+var currentNodes = 0;
+var currentEdges = 0;
+
+refreshGraphData();
